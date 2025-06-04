b0c02aed1cc96b0d27dd3b8d9ff3d28fc91974c6
rohxn-rai <rohan.rai932@outlook.com>
2025-06-04 08:04:45 -0400

    added basic error handling

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const errorHandler = (
   err: Error,
@@ -6,10 +8,25 @@ const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went worng", err);
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      if (error.type === "field") {
+        return {
+          message: error.msg,
+          field: error.path,
+        };
+      }
+    });
+
+    return res.status(400).send({ errors: formattedErrors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
 
   res.status(400).send({
-    message: "Something went wrong!",
+    message: err.message,
   });
 };
 
