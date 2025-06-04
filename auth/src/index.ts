3b38fa2c3a2c613baa44fe8fe0289455d5552f79
rohxn-rai <rohan.rai932@outlook.com>
2025-06-04 05:25:56 -0400

    initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,17 @@
+import express, { json } from "express";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.listen(8000, () => {
+  console.log("Listening on port 8000");
+});
