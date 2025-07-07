5c96eb01306a44c4ec751ef5763057f4936f8dc3
rohxn-rai <rohan.rai932@outlook.com>
2025-07-07 04:01:11 -0400

    attached requireauth middleware to current-user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
 
 import { currentUser } from "../middlewares/current-users";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currentUser, ((
+router.get("/api/users/currentuser", currentUser, requireAuth, ((
   req: Request,
   res: Response
 ) => {
