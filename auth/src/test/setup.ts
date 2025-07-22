d1c5cd2f20129c060ad6d4188adf346b1d0ed2cf
rohxn-rai <rohan.rai932@outlook.com>
2025-07-22 05:32:11 -0400

    added signup function - to avoid signing up for each test again and again

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
 import { app } from "../app";
+
+declare global {
+  var signup: () => Promise<string[]>;
+}
 
 let mongo: any;
 
@@ -25,3 +30,20 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signup = async () => {
+  const email = "test@test.com";
+  const password = "Password";
+
+  const response = await request(app).post("/api/users/signup").send({
+    email,
+    password,
+  });
+
+  const cookie = response.get("Set-Cookie");
+
+  if (!cookie) {
+    throw new Error("Failed to get cookie from response");
+  }
+  return cookie;
+};
