22adc3c7f9535824a8f6bfa4039075706e28b037
rohxn-rai <rohan.rai932@outlook.com>
2025-07-22 05:31:26 -0400

    moved require auth function from the currentuser route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
 
 import { currentUser } from "../middlewares/current-user";
-import { requireAuth } from "../middlewares/require-auth";
+// import { requireAuth } from "../middlewares/require-auth";
+// helper middleware for other services
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currentUser, requireAuth, ((
+router.get("/api/users/currentuser", currentUser, ((
   req: Request,
   res: Response
 ) => {
