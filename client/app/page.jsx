0ead18cb4adc0141e3d02389e7232e607d5bc49d
rohxn-rai <rohan.rai932@outlook.com>
2025-07-23 08:20:53 -0400

    created landing page - with basic user details using helper function

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,5 +1,24 @@
-const HomePage = () => {
-  return <div className="text-2xl">HomePage</div>;
+import { buildClient } from "@/helpers/build-client";
+
+const HomePage = async () => {
+  const client = await buildClient();
+
+  const { data } = await client.get("/api/users/currentuser");
+  const { currentUser } = data;
+
+  return (
+    <div className="container flex mx-auto pt-24 pb-12">
+      <div className="max-w-2xl w-full mx-auto">
+        {currentUser !== null && (
+          <div>
+            <p>{currentUser.id}</p>
+            <p>{currentUser.email}</p>
+            <p>{currentUser.iat}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
