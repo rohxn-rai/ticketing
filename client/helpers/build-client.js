d12c85f343b953984f605bb9cc8b893637b31ed6
rohxn-rai <rohan.rai932@outlook.com>
2025-07-23 08:20:17 -0400

    updated build-client helper

--- client/helpers/build-client.js
+++ client/helpers/build-client.js
@@ -4,9 +4,11 @@ import { headers } from "next/headers";
 const buildClient = async () => {
   const headersList = await headers();
 
+  const baseURL = process.env.NEXT_BASE_URL || "/";
+
   const cookie = headersList.get("cookie");
   return axios.create({
-    baseURL: process.env.NEXT_BASE_URL || "/",
+    baseURL,
     headers: process.env.NEXT_BASE_URL ? headersList : { cookie },
   });
 };
