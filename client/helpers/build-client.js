2bdf82f5580c63175d77865a2fea024bc27be41f
rohxn-rai <rohan.rai932@outlook.com>
2025-07-23 10:07:52 -0400

    modified helper function

--- client/helpers/build-client.js
+++ client/helpers/build-client.js
@@ -4,12 +4,11 @@ import { headers } from "next/headers";
 const buildClient = async () => {
   const headersList = await headers();
 
-  const baseURL = process.env.NEXT_BASE_URL || "/";
+  const baseURL = process.env.NEXT_BASE_URL;
 
-  const cookie = headersList.get("cookie");
   return axios.create({
     baseURL,
-    headers: process.env.NEXT_BASE_URL ? headersList : { cookie },
+    headers: headersList,
   });
 };
 
