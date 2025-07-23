df19f0c73105f45d5b99abd371647fc7106d6d2b
rohxn-rai <rohan.rai932@outlook.com>
2025-07-23 10:08:40 -0400

    added current user util function - to share current user dertails

---
+++ client/utils/getCurrentUser.js
@@ -0,0 +1,12 @@
+import { buildClient } from "@/helpers/build-client";
+
+export const getCurrentUser = async () => {
+  const client = await buildClient();
+
+  try {
+    const { data } = await client.get("/api/users/currentuser");
+    return data.currentUser;
+  } catch (error) {
+    return null;
+  }
+};
