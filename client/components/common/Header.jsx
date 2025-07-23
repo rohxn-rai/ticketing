33720065ae6541b539555bdcda2a8b87fdb4022e
rohxn-rai <rohan.rai932@outlook.com>
2025-07-23 11:25:49 -0400

    created Header

---
+++ client/components/common/Header.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import NavHeader from "./NavHeader";
+import { getCurrentUser } from "@/utils/getCurrentUser";
+import ThemeToggle from "../theme/theme-toggle";
+
+const Header = async () => {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <header className="border-b border-border py-3 sticky top-0">
+      <nav className="container mx-auto flex flex-row justify-between">
+        <Link href="/" className="text-2xl font-bold">
+          GitTix
+        </Link>
+
+        <div className="flex flex-row gap-8 align-middle">
+          <NavHeader currentUser={currentUser} />
+          <ThemeToggle />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
