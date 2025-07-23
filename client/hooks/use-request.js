3f9879baaf2a7d8f709559e5c94ee6fd6ec863c3
rohxn-rai <rohan.rai932@outlook.com>
2025-07-23 04:47:26 -0400

    updated custom use-request hook

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,31 +1,25 @@
 import axios from "axios";
 import { useState } from "react";
 
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import ErrorAlert from "@/components/common/errorAlert";
 
-import { FaCircleInfo } from "react-icons/fa6";
-
-const useRequest = ({ url, method, body }) => {
+const useRequest = ({ url, method, body, message, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
     try {
       setErrors(null);
+
       const response = await axios[method](url, body);
+
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
       return response.data;
     } catch (err) {
       setErrors(
-        <Alert variant="destructive">
-          <FaCircleInfo />
-          <AlertTitle>Error!</AlertTitle>
-          <AlertDescription>
-            <ul className="my-0">
-              {err.response.data.errors.map((err, index) => (
-                <li key={index}>{err.message}</li>
-              ))}
-            </ul>
-          </AlertDescription>
-        </Alert>
+        <ErrorAlert errors={err.response.data.errors} message={message} />
       );
     }
   };
