0f776b2c89acfa0c8a991b08a92c70aa1aee64c1
rohxn-rai <rohan.rai932@outlook.com>
2025-07-23 10:09:37 -0400

    added authenticated and non authenticated page - bad idea to hard code but it is just a boiler plate

---
+++ client/components/home/AuthHomePage.jsx
@@ -0,0 +1,5 @@
+const AuthHomePage = () => {
+  return <h1 className="text-2xl">You are signed in!</h1>;
+};
+
+export default AuthHomePage;
---
+++ client/components/home/NoAuthHomePage.jsx
@@ -0,0 +1,5 @@
+const NoAuthHomePage = () => {
+  return <h1 className="text-2xl">You are not signed in!</h1>;
+};
+
+export default NoAuthHomePage;
