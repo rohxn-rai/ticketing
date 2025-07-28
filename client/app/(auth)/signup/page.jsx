0f4f0f80b5b60860a762d391513fd55f3f1b4306
rohxn-rai <rohan.rai932@outlook.com>
2025-07-28 04:24:31 -0400

    updated signin and signup routes with new layout

--- client/app/(auth)/signup/page.jsx
+++ client/app/(auth)/signup/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { replacePath } from "@/actions/redirect";
+import { replacePath } from "@/actions/replacePath";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -34,44 +34,42 @@ const SignupPage = () => {
   };
 
   return (
-    <div className="container flex mx-auto pt-24 pb-12">
-      <div className="max-w-2xl w-full mx-auto">
-        <form className="mx-6" onSubmit={onSubmit}>
-          <h1 className="text-center text-4xl">Sign Up</h1>
+    <div className="max-w-2xl w-full mx-auto">
+      <form className="mx-6" onSubmit={onSubmit}>
+        <h1 className="text-center text-4xl">Sign Up</h1>
 
-          {errors}
+        {errors}
 
-          <div className="flex flex-col gap-4 mt-6">
-            <div className="flex flex-col gap-2">
-              <label htmlFor="email">Email Address</label>
-              <Input
-                id="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                type="text"
-                autoComplete="email"
-              />
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <label htmlFor="password">Password</label>
-              <Input
-                id="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                autoComplete="new-password"
-              />
-            </div>
-
-            <Button size="lg" type="submit" disabled={loading}>
-              {loading ? "Signing up…" : "Sign Up"}
-            </Button>
+        <div className="flex flex-col gap-4 mt-6">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email">Email Address</label>
+            <Input
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              autoComplete="email"
+            />
           </div>
-        </form>
-      </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password">Password</label>
+            <Input
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              autoComplete="new-password"
+            />
+          </div>
+
+          <Button size="lg" type="submit" disabled={loading}>
+            {loading ? "Signing up…" : "Sign Up"}
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
