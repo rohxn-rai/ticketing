cfec82ecf776ff4879ef8c2290beeaafd0d88994
rohxn-rai <rohan.rai932@outlook.com>
2025-08-01 05:26:39 -0400

    replace import with the new custom installed package

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -6,8 +6,8 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
-import errorHandler from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "@ticketing-backend-packages/common";
+import { NotFoundError } from "@ticketing-backend-packages/common";
 
 dotenv.config();
 
