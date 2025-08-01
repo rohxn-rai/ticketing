72df5e97c870384d6e6c24d4da6da59c54a25e55
rohxn-rai <rohan.rai932@outlook.com>
2025-08-01 08:30:42 -0400

    replaced mongodb uri with env variable

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,9 +8,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI must be defined");
+  }
 
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB :]");
   } catch (err) {
     console.log(err);
