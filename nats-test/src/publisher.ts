7f38c72993b68b767a63ed939a5c7890719fa95d
rohxn-rai <rohan.rai932@outlook.com>
2025-09-05 07:11:55 -0400

    created nats publisher

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,15 @@
+import nats from "node-nats-streaming"
+
+const stan  = nats.connect("ticketing","abc",{
+  url: "http://localhost:4222"
+});
+
+stan.on("connect", () => {
+  console.log("Publisher connected to NATS");
+  
+  const data = {
+    id: "123",
+    title: "concert",
+    price: 200
+  };
+})
