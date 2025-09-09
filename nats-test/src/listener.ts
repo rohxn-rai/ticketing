b8d45f219127ffe8bb34bcb4d96a3466da9625c1
rohxn-rai <rohan.rai932@outlook.com>
2025-09-09 10:36:27 -0400

    added unique message ack and closing event handler

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,22 +1,42 @@
 import nats, { Message } from "node-nats-streaming"
-import {randomBytes} from "crypto";
+import { randomBytes } from "crypto";
 
-console.clear()
+console.clear ()
 
-const stan = nats.connect("ticketing", randomBytes(4).toString('hex') , {
-  url: "http://localhost:4222"
-})
+const stan = nats.connect (
+  "ticketing",
+  randomBytes ( 4 ).toString ( 'hex' )
+  , {
+    url : "http://localhost:4222"
+  } )
 
-stan.on("connect", ()=>{
-  console.log("Listener connected to NATS")
+stan.on ( "connect", () => {
+  console.log ( "Listener connected to NATS" )
   
-  const subscription = stan.subscribe("ticket:created","orders-service-queue-group")
+  stan.on ( "close", () => {
+    console.log ( "Listener disconnected" )
+    process.exit ()
+  } )
   
-  subscription.on("message", (msg:Message)=>{
-    const data  = msg.getData()
+  const options = stan
+    .subscriptionOptions ()
+    .setManualAckMode ( true )
+  const subscription = stan.subscribe (
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  )
+  
+  subscription.on ( "message", ( msg : Message ) => {
+    const data = msg.getData ()
     
-    if(typeof data === "string") {
-      console.log ( `Received event #${msg.getSequence()}, with data: ${data}` );
+    if ( typeof data === "string" ) {
+      console.log ( `Received event #${ msg.getSequence () } at ${ msg.getTimestamp () }, with data: ${ data }` );
     }
-  })
-})
+    
+    msg.ack ()
+  } )
+} )
+
+process.on ( "SIGINT", () => stan.close () )
+process.on ( "SIGTERM", () => stan.close () )
