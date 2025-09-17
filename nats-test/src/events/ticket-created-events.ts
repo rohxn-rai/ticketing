545ba2e4a442ffc6847a97c3d9c665a00457d61d
rohxn-rai <rohan.rai932@outlook.com>
2025-09-17 04:29:35 -0400

    created TicketCreatedEvents interface

---
+++ nats-test/src/events/ticket-created-events.ts
@@ -0,0 +1,10 @@
+import { Subjects } from "./subjects";
+
+export interface TicketCreatedEvents {
+  subject : Subjects.TicketCreated;
+  data : {
+    id : string;
+    title : string;
+    price : number;
+  }
+}
