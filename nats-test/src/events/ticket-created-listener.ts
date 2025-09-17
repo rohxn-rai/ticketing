8cb82f555e8ea22af80f2f6ec7427b06940c206e
rohxn-rai <rohan.rai932@outlook.com>
2025-09-17 04:30:13 -0400

    refactored code with the new interface - for better type casting

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,18 @@
 import { Listener } from "./base-listener";
 import { Message } from "node-nats-streaming";
+import { TicketCreatedEvents } from "./ticket-created-events";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvents> {
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = "payments-service-queue-group";
   
-  onMessage ( data : any, msg : Message ) {
+  onMessage ( data : TicketCreatedEvents["data"], msg : Message ) {
     console.log ( 'Event data!', data );
+    
+    console.log ( data.id )
+    console.log ( data.title )
+    console.log ( data.price )
     msg.ack ();
   }
 }
