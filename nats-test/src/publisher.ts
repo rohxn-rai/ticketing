e212fde107f54a19de134bf6275bc1e7fd3b9837
rohxn-rai <rohan.rai932@outlook.com>
2025-09-17 05:53:09 -0400

    refactored the old publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,21 +1,23 @@
 import nats from "node-nats-streaming"
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
-console.clear()
+console.clear ()
 
-const stan  = nats.connect("ticketing","abc",{
-  url: "http://localhost:4222"
-});
+const stan = nats.connect ( "ticketing", "abc", {
+  url : "http://localhost:4222"
+} );
 
-stan.on("connect", () => {
-  console.log("Publisher connected to NATS");
+stan.on ( "connect", async () => {
+  console.log ( "Publisher connected to NATS" );
   
-  const data = JSON.stringify({
-    id: "123",
-    title: "concert",
-    price: 200
-  });
-  
-  stan.publish("ticket:created", data, ()=>{
-    console.log("Event ticket created published");
-  })
-})
+  const publisher = new TicketCreatedPublisher ( stan )
+  try {
+    await publisher.publish ( {
+      id : "123",
+      title : "concert",
+      price : 200
+    } )
+  } catch ( err ) {
+    console.error ( err )
+  }
+} )
