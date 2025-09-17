4f7863266dabcd68ff1578f8755a213723488531
rohxn-rai <rohan.rai932@outlook.com>
2025-09-17 06:25:59 -0400

    changed from const to var

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -4,46 +4,46 @@ import request from "supertest";
 import { app } from "../app";
 
 declare global {
-  const signup: () => Promise<string[]>;
+  var signup : () => Promise<string[]>;
 }
 
-let mongo: any;
+let mongo : any;
 
-beforeAll(async () => {
+beforeAll ( async () => {
   process.env.JWT_KEY = "testing_JWT_KEY";
+  
+  mongo = await MongoMemoryServer.create ();
+  const mongoUri = mongo.getUri ();
+  
+  await mongoose.connect ( mongoUri, {} );
+} );
 
-  mongo = await MongoMemoryServer.create();
-  const mongoUri = mongo.getUri();
-
-  await mongoose.connect(mongoUri, {});
-});
-
-beforeEach(async () => {
-  const collections = await mongoose.connection.db!.collections();
-
-  for (let collection of collections) {
-    await collection.deleteMany({});
+beforeEach ( async () => {
+  const collections = await mongoose.connection.db!.collections ();
+  
+  for ( let collection of collections ) {
+    await collection.deleteMany ( {} );
   }
-});
+} );
 
-afterAll(async () => {
-  await mongo.stop();
-  await mongoose.connection.close();
-});
+afterAll ( async () => {
+  await mongo.stop ();
+  await mongoose.connection.close ();
+} );
 
-const signup = async () => {
+global.signup = async () => {
   const email = "test@test.com";
   const password = "Password";
-
-  const response = await request(app).post("/api/users/signup").send({
+  
+  const response = await request ( app ).post ( "/api/users/signup" ).send ( {
     email,
     password,
-  });
-
-  const cookie = response.get("Set-Cookie");
-
-  if (!cookie) {
-    throw new Error("Failed to get cookie from response");
+  } );
+  
+  const cookie = response.get ( "Set-Cookie" );
+  
+  if ( !cookie ) {
+    throw new Error ( "Failed to get cookie from response" );
   }
   return cookie;
 };
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -3,46 +3,46 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
 declare global {
-  const signup: () => string[];
+  var signup : () => string[];
 }
 
-let mongo: any;
+let mongo : any;
 
-beforeAll(async () => {
+beforeAll ( async () => {
   process.env.JWT_KEY = "testing_JWT_KEY";
+  
+  mongo = await MongoMemoryServer.create ();
+  const mongoUri = mongo.getUri ();
+  
+  await mongoose.connect ( mongoUri, {} );
+} );
 
-  mongo = await MongoMemoryServer.create();
-  const mongoUri = mongo.getUri();
-
-  await mongoose.connect(mongoUri, {});
-});
-
-beforeEach(async () => {
-  const collections = await mongoose.connection.db!.collections();
-
-  for (let collection of collections) {
-    await collection.deleteMany({});
+beforeEach ( async () => {
+  const collections = await mongoose.connection.db!.collections ();
+  
+  for ( let collection of collections ) {
+    await collection.deleteMany ( {} );
   }
-});
+} );
 
-afterAll(async () => {
-  await mongo.stop();
-  await mongoose.connection.close();
-});
+afterAll ( async () => {
+  await mongo.stop ();
+  await mongoose.connection.close ();
+} );
 
-const signup = () => {
+global.signup = () => {
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
-    email: "test@test.com",
+    id : new mongoose.Types.ObjectId ().toHexString (),
+    email : "test@test.com",
   };
-
-  const token = jwt.sign(payload, process.env.JWT_KEY!, { expiresIn: "1h" });
-
-  const session = { jwt: token };
-
-  const sessionJSON = JSON.stringify(session);
-
-  const base64 = Buffer.from(sessionJSON).toString("base64");
-
-  return [`session=${base64}`];
+  
+  const token = jwt.sign ( payload, process.env.JWT_KEY!, { expiresIn : "1h" } );
+  
+  const session = { jwt : token };
+  
+  const sessionJSON = JSON.stringify ( session );
+  
+  const base64 = Buffer.from ( sessionJSON ).toString ( "base64" );
+  
+  return [ `session=${ base64 }` ];
 };
