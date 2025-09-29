fd2e59cdd4f9dbe60b2065f9dfddcec3c83c8a44
rohxn-rai <rohan.rai932@outlook.com>
2025-09-29 04:15:28 -0400

    created orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,41 @@
+import express, { json } from "express";
+import cookieSession from "cookie-session";
+
+import {
+  currentUser,
+  errorHandler,
+  NotFoundError,
+} from "@rohxnrai/todo-backend";
+
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.use(async (req, res, next) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
+
+export { app };
