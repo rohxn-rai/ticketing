6c812bc0cc0fcd37c90af736679fd3fa17a0e277
rohxn-rai <rohan.rai932@outlook.com>
2025-10-09 06:17:16 -0400

    refactoring delete orders route to cancel orders route

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -8,34 +8,34 @@ import {
 } from "@rohxnrai/todo-backend";
 
 import { indexOrderRouter } from "./routes/index";
-import { deleteOrderRouter } from "./routes/delete";
+import { cancelOrderRouter } from "./routes/cancel";
 import { showOrderRouter } from "./routes/show";
 import { newOrderRouter } from "./routes/new";
 
-const app = express();
+const app = express ();
 
-app.set("trust proxy", true);
+app.set ( "trust proxy", true );
 
-app.use(json());
+app.use ( json () );
 
-app.use(
-  cookieSession({
-    signed: false,
-    secure: process.env.NODE_ENV !== "test",
-  })
+app.use (
+  cookieSession ( {
+    signed : false,
+    secure : process.env.NODE_ENV !== "test",
+  } )
 );
 
-app.use(currentUser);
+app.use ( currentUser );
 
-app.use(indexOrderRouter);
-app.use(deleteOrderRouter);
-app.use(showOrderRouter);
-app.use(newOrderRouter);
+app.use ( indexOrderRouter );
+app.use ( cancelOrderRouter );
+app.use ( showOrderRouter );
+app.use ( newOrderRouter );
 
-app.use(async (req, res, next) => {
-  next(new NotFoundError());
-});
+app.use ( async ( req, res, next ) => {
+  next ( new NotFoundError () );
+} );
 
-app.use(errorHandler);
+app.use ( errorHandler );
 
 export { app };
---
+++ orders/src/routes/cancel.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from "express";
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth
+} from "@rohxnrai/todo-backend";
+import { Order, OrderStatus } from "../models/order";
+import mongoose from "mongoose";
+
+const router = express.Router ();
+
+router.patch (
+  "/api/orders/:orderId",
+  requireAuth,
+  async (
+    req : Request,
+    res : Response
+  ) => {
+    const { orderId } = req.params
+    if ( !mongoose.Types.ObjectId.isValid ( orderId ) )
+      throw new BadRequestError ( "Not a valid Order ID." );
+    
+    const order = await Order.findById ( orderId )
+      .populate ( "ticket" )
+    
+    if ( !order )
+      throw new NotFoundError ();
+    
+    if ( order.userId !== req.currentUser!.id )
+      throw new NotAuthorizedError ();
+    
+    order.status = OrderStatus.Cancelled;
+    
+    await order.save ();
+    
+    res.send ( {} );
+  } )
+
+export { router as cancelOrderRouter }
